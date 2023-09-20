832426e8f2bdc854815b63fa468377fb2fdd53a9
tianT-800 <sebastianrinaldi10@hotmail.com>
2023-09-20 11:14:33 -0300

    remove-branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -51,17 +51,17 @@ export const Badge = (props: Props) => {
       "text-align": 'center',
       color: props.poweredByTextColor ?? defaultTextColor,
       "background-color": props.badgeBackgroundColor ?? '#ffffff'
-    }}>Powered by
+    }}>Tecnologia impulsada por
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://www.chatsappai.com/'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> ChatsappAI.com</span>
       </a>
     </span>
   )
